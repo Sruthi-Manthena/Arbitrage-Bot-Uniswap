771476e8d78d4c118aead97ced32efaeccf0ee20
Sruthi-Manthena <84638439+Sruthi-Manthena@users.noreply.github.com>
2023-02-26 20:22:23 +0530

    Update Shebaswap.js

--- scripts/Shebaswap.js
+++ scripts/Shebaswap.js
@@ -16,12 +16,6 @@ async function getSellPrice(account, inputAmt) {
         "0x8faf958E36c6970497386118030e6297fFf8d275",
         account
     )
-    const { reserve0, reserve1 } = await ShebaPair.getReserves()
-    const ShebaRouter = await ethers.getContractAt(
-        "IUniswapV2Router02",
-        "0x03f7724180AA6b939894B5Ca4314783B0b36b329",
-        account
-    )
     const sellPrice = (
         await ShebaRouter.getAmountIn(inputAmt, reserve0, reserve1)
     ).toString()

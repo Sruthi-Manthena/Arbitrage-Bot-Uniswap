c285b6ace81da21aa93cd063effef1bbbab3f15d
Sruthi-Manthena <84638439+Sruthi-Manthena@users.noreply.github.com>
2023-03-01 16:25:30 +0530

    Update AggregatorV3Interface.sol

--- contracts/interfaces/AggregatorV3Interface.sol
+++ contracts/interfaces/AggregatorV3Interface.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: MIT
+// Specify the license under which the code is released
 pragma solidity ^0.8.0;
+// Specify the version of Solidity that the code is written in
 
+// Define an interface for an oracle contract that provides price data for an asset
 interface AggregatorV3Interface {
+    // This function returns the number of decimal places that the price of the asset is expressed in
     function decimals() external view returns (uint8);
 
+    // This function returns a string that describes the asset whose price is being reported
     function description() external view returns (string memory);
 
+    // This function returns a number that represents the version of the contract that is currently deployed on the blockchain
     function version() external view returns (uint256);
 
-    // getRoundData and latestRoundData should both raise "No data present"
-    // if they do not have data to report, instead of returning unset values
-    // which could be misinterpreted as actual reported values.
+    // This function retrieves data about a specific round of price data
     function getRoundData(uint80 _roundId)
         external
         view
@@ -22,6 +26,7 @@ interface AggregatorV3Interface {
             uint80 answeredInRound
         );
 
+    // This function retrieves data about the most recent round of price data
     function latestRoundData()
         external
         view
